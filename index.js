5d3e3b2dfdaef041a7a3437f313b43536ccccade
SHEKU J SESAY <jeremiahsheku2000@gmail.com>
2022-09-17 02:44:10 +0000

    let calculate two numbers

---
+++ index.js
@@ -0,0 +1,6 @@
+function myassignment() {
+    number1 = parseInt(document.getElementById('number1').value);
+    number2 = parseInt(document.getElementById('number2').value);
+    add = number1 + number2;
+    document.write(add);
+}
